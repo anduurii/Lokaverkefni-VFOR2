097c2ce75c34b8407a8179e29cb8ff1dabc49da1
anduurii <karlandri2006@hotmail.com>
2026-03-03 10:37:14 +0000

    server.js komið

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();     
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use((req, res, next) => {
+    res.status(404).render('404', { title: 'Site Not Found' });
+});
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Oopsie');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server keyrir á http://localhost:${PORT}/`);
+});
