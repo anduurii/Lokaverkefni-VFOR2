db8a927ab13ff1b9b9b98315ecb19c13b5e30f27
anduurii <karlandri2006@hotmail.com>
2026-03-03 11:38:54 +0000

    byrjun á views

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ app.set('views', path.join(__dirname, 'src', 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home' });
+});
+
+
+
+
+
 app.use((req, res, next) => {
     res.status(404).render('404', { title: 'Site Not Found' });
 });
