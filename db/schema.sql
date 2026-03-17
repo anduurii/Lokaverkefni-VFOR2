5f09a1af3e41e97b1c782862775679040efcd7af
anduurii <karlandri2006@hotmail.com>
2026-03-17 10:38:37 +0000

    teikning og gögn

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE CHECK(email LIKE "%_@_%._%"),
+    password VARCHAR(255) NOT NULL,
+    created TIMESTAMP,
+    last_login TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS teams (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -6,8 +14,28 @@ CREATE TABLE IF NOT EXISTS teams (
     description TEXT
 );
 
+CREATE TABLE IF NOT EXISTS players (
+    id SERIAL PRIMARY KEY,
+    nickname VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    nationality VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    team_id INT
+);
+
+
+
 INSERT INTO teams (title, logo, region, description) VALUES
 ("Vitality", "BREYTA", "Europe", "Team Vitality is a top-tier French esports organization 
-whose CS2 squad is recognized for dynamic strategies, 
-fast-paced aggression, and consistently competing at the forefront of the global scene."),
-("Spirit", "BREYTA", "Europe", "")
+whose CS2 squad is recognized for dynamic strategies, fast-paced aggression, 
+and consistently competing at the forefront of the global scene."),
+("Spirit", "BREYTA", "Europe", "Team spirit in CS2 means working together through communication, 
+trust, and coordination to achieve victory as a team."),
+("Falcons", "BREYTA", "Europe", "Team Falcons is a rising esports organization known for its strong roster, 
+competitive ambition, and growing impact in Counter-Strike 2.");
+
+
+INSERT INTO players (nickname, first_name, last_name, nationality, date_of_birth, role, team_id) VALUES
+()
