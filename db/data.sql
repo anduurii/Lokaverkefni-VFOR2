3dff67c965da57443859fe3eb3779b9a71a2355e
anduurii <karlandri2006@hotmail.com>
2026-03-18 10:40:57 +0000

    gagnagrunn tenging komið

--- db/data.sql
+++ db/data.sql
@@ -2,11 +2,31 @@ INSERT INTO teams (title, logo, region, description) VALUES
 ("Vitality", "BREYTA", "Europe", "Team Vitality is a top-tier French esports organization 
 whose CS2 squad is recognized for dynamic strategies, fast-paced aggression, 
 and consistently competing at the forefront of the global scene."),
+
 ("Spirit", "BREYTA", "Europe", "Team spirit in CS2 means working together through communication, 
 trust, and coordination to achieve victory as a team."),
+
 ("Falcons", "BREYTA", "Europe", "Team Falcons is a rising esports organization known for its strong roster, 
 competitive ambition, and growing impact in Counter-Strike 2.");
 
 
 INSERT INTO players (nickname, first_name, last_name, nationality, date_of_birth, role, team_id) VALUES
-()
+("ZywOo", "Mathieu", "Herbaut", "France", "2000-11-09", "Rifler/AWPer", 1),
+("apEX", "Dan", "Madesclaire", "France", "1993-02-22", "In-Game Leader", 1),
+("ropz", "Robin", "Kool", "Estonia", "1999-12-22", "Lurker", 1),
+("mezii", "William", "Merriman", "United Kingdom", "1998-10-15", "Rifler", 1),
+("flameZ", "Shahar", "Shushan", "Israel", "2003-06-22", "Entry", 1),
+
+("donk", "Danil", "Kryshkovets", "Russia", "2007-01-25", "Entry", 2),
+("magixx", "Boris", "Vorobyev", "Russia", "2003-06-03", "In-Game Leader", 2),
+("zont1x", "Myroslav", "Plakhotia", "Ukraine", "2005-07-20", "Rifler", 2),
+("sh1ro", "Dmitriy", "Sokolov", "Russia", "2001-07-15", "AWPer", 2),
+("tN1R", "Andrey", "Tatarinovich", "Belarus", "2001-02-14", "Rifler", 2),
+
+("m0NESY", "Ilya", "Osipov", "Russia", "2005-05-01", "AWPer", 3),
+("kyousuke", "Maksim", "Lukin", "Russia", "2008-01-30", "Rifler", 3),
+("NiKo", "Nikola", "Kovač", "Bosnia and Herzegovina", "1997-02-16", "Rifler", 3),
+("kyxsan", "Damjan", "Stoilkovski", "North Macedonia", "2000-05-26", "In-Game Leader", 3),
+("TeSeS", "René", "Madsen", "Denmark", "2000-12-12", "Rifler", 3);
+
+
